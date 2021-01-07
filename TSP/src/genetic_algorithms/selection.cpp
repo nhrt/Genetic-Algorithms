82e3ebbb38b68bd7abeb4a5d6310fbceaeda1f82
nhrt <hartingerniklas@gmail.com>
2021-01-07 08:59:28 +0100

    Reversed selection is unnecessary
    Is unnecessary because of the new reversed fitness function.

--- TSP/src/genetic_algorithms/selection.cpp
+++ TSP/src/genetic_algorithms/selection.cpp
@@ -37,36 +37,3 @@ Population selection_sotf(Population &p_old, Population &p_new) {
 
     return result;
 }
-
-Population selection_sotf_reversed(Population &p_old, Population &p_new){
-    if (p_old.size() != p_new.size()) {
-        std::cerr << "Populations have different sizes" << std::endl;
-        exit(1);
-    }
-    int size = (int) p_old.size();
-
-    p_old.calc_population_fitness();
-    p_new.calc_population_fitness();
-
-    Population result = Population(p_old.get_idx_start(), p_old.get_distances());
-
-    std::vector<Individual> individuals_old = p_old.get_individuals();
-    std::vector<Individual> individuals_new = p_new.get_individuals();
-
-    std::sort(individuals_new.begin(), individuals_new.end());
-    std::sort(individuals_old.begin(), individuals_old.end());
-
-    int offset_old = 0;
-    int offset_new = 0;
-    for (int i = 0; i < size; ++i) {
-        if (individuals_old.at(offset_old) < individuals_new.at(offset_new)) {
-            result.add_individual(individuals_old.at(offset_old));
-            offset_old++;
-        } else {
-            result.add_individual(individuals_new.at(offset_new));
-            offset_new++;
-        }
-    }
-
-    return result;
-}
