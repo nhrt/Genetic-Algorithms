ac6d9c25ca6d0c6841c98b8b55f1ed682e5e8ec3
nhrt <hartingerniklas@gmail.com>
2021-01-07 09:01:45 +0100

    Scenario not necessary anymore

--- TSP/tests/genetic_algorithms/test_selection.cpp
+++ TSP/tests/genetic_algorithms/test_selection.cpp
@@ -21,7 +21,18 @@ SCENARIO("Test Genetic Algorithms selection sotf", "[genetic_algorithms_selectio
         REQUIRE(p_best.get_last_calculates_population_fitness() >= population2.get_last_calculates_population_fitness());
     }
 
+    for (int i = 0; i < 100; ++i) {
+        Population population1 = Population(5, 5, 1, rating_reversed, fitness_reversed, distances);
+        Population population2 = Population(5, 5, 1, rating_reversed, fitness_reversed, distances);
+        Population p_best = selection_sotf(population1, population2);
+
+        p_best.calc_population_fitness();
+        REQUIRE(p_best.get_last_calculates_population_fitness() >= population1.get_last_calculates_population_fitness());
+        REQUIRE(p_best.get_last_calculates_population_fitness() >= population2.get_last_calculates_population_fitness());
+    }
+
 }
+/*
 SCENARIO("Test Genetic Algorithms selection sotf reversed", "[genetic_algorithms_selection.cpp]") {
     std::string file_distances = "distances";
     std::string location = "../../data/cities/";
@@ -38,3 +49,4 @@ SCENARIO("Test Genetic Algorithms selection sotf reversed", "[genetic_algorithms
         REQUIRE(p_best.get_last_calculates_population_fitness() <= population2.get_last_calculates_population_fitness());
     }
 }
+ */
