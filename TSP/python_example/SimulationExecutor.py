9ea6b7aac17eb73cc900340ff96b76df41dd8a48
Jannis Weber <jannis.weber@mni.thm.de>
2021-01-14 11:44:26 +0100

    refactor simulation result list

--- TSP/python_example/SimulationExecutor.py
+++ TSP/python_example/SimulationExecutor.py
@@ -8,9 +8,12 @@ class SimulationExecutor:
     def __init__(self, simulator):
         self.simulator = simulator
         self.generation = 0
+        self.result_list = []
 
-    def simulate(self) -> 'SimulationResult':
+    def simulate(self, store_result: bool = True) -> 'SimulationResult':
         res_fitness: List[int, int, int] = self.simulator.simulate()
         result: SimulationResult = SimulationResult(res_fitness[0], res_fitness[1], res_fitness[2], self.generation)
+        if store_result:
+            self.result_list.append(result)
         self.generation += 1
         return result
--- TSP/python_example/Simulation_Test.py
+++ TSP/python_example/Simulation_Test.py
@@ -1,11 +1,9 @@
-from typing import List
-
 from Simulator_Wrapper import *
 from SimulationResult import SimulationResult
 from Plotter import Plotter
 from SimulationExecutor import SimulationExecutor
 
-generations = 1000
+generations = 50000
 cities = 59
 mutation = 10
 size_individual = 10
@@ -15,13 +13,10 @@ s = Simulator(
     cities, size_individual, generations, mutation,
     Crossover_Algorithm.Edge_Recombination, Marriage_Algorithm.Roulette_Reversed, Mutation_Algorithm.Delete_Shift, Selection_Algorithm.SOFT)
 
-sr_list: List[SimulationResult] = []
-
 executor = SimulationExecutor(s)
 for generation in range(generations):
     sr: SimulationResult = executor.simulate()
-    sr_list.append(sr)
     print(sr)
 
-Plotter.plot(sr_list, use_distances=False)
-Plotter.plot(sr_list, use_distances=True)
+Plotter.plot(executor.result_list, use_distances=False)
+Plotter.plot(executor.result_list, use_distances=True)
