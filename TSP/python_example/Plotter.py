7807483d38c6191994d8338bdf171b6aad0ce155
Jannis Weber <jannis.weber@mni.thm.de>
2021-01-14 12:02:48 +0100

    refactor plotter, add legend

--- TSP/python_example/Plotter.py
+++ TSP/python_example/Plotter.py
@@ -13,18 +13,19 @@ class Plotter:
     def plot(sr: List[SimulationResult], use_distances: bool = False):
         xs = [x.generation for x in sr]
         if use_distances:
+            ys_best = [x.get_distance_highest() for x in sr]
             ys_avg = [x.get_distance_avg() for x in sr]
-            ys_lowest = [x.get_distance_lowest() for x in sr]
-            ys_highest = [x.get_distance_highest() for x in sr]
+            ys_worst = [x.get_distance_lowest() for x in sr]
         else:
+            ys_best = [x.highest_fitness for x in sr]
             ys_avg = [x.avg_fitness for x in sr]
-            ys_lowest = [x.lowest_fitness for x in sr]
-            ys_highest = [x.highest_fitness for x in sr]
+            ys_worst = [x.lowest_fitness for x in sr]
         title = "{} über {} Generationen".format("Distanzen" if use_distances else "Fitness", max(xs)-min(xs)+1)
 
-        plt.plot(xs, ys_avg, color=(0, 0, 1, 1))
-        plt.plot(xs, ys_lowest, color=(1, 0, 0, 1))
-        plt.plot(xs, ys_highest, color=(0, 1, 0, 1))
+        plt.plot(xs, ys_best, color=(0, 1, 0, 1), label="best")
+        plt.plot(xs, ys_avg, color=(0, 0, 1, 1), label="average")
+        plt.plot(xs, ys_worst, color=(1, 0, 0, 1), label="worst")
         plt.title(title)
+        plt.legend()
 
         plt.show()
