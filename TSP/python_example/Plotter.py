504883ce29921b9e2b8acb53a9c7ab6a6b1c164a
Jannis Weber <jannis.weber@mni.thm.de>
2021-01-20 00:27:45 +0100

    refactor string representation of a simulation result list

--- TSP/python_example/Plotter.py
+++ TSP/python_example/Plotter.py
@@ -34,11 +34,9 @@ class Plotter:
 			all_generations: List[int] = list(map(lambda sr: sr.generation, simulations[0].results))
 			first_generation: int = min(all_generations)
 			last_generation: int = max(all_generations)
-			title: str = "{} über {} Generationen bei {} Städten\n{} Ind., {}, {}, {} ({}%), {}".format(
+			title: str = "{} über {} Generationen bei {} Städten\n{}".format(
 				"Distanz" if use_distances else "Fitness", last_generation - first_generation + 1,
-				simulations[0].cities, simulations[0].population_size, get_marriage_name(simulations[0].marriage),
-				get_crossover_name(simulations[0].crossover), get_mutation_name(simulations[0].mutation),
-				simulations[0].mutation_rate, get_selection_name(simulations[0].selection))
+				simulations[0].cities, str(simulations[0]))
 			plt.title(title)
 			plt.legend()
 			plt.show()
@@ -50,12 +48,7 @@ class Plotter:
 					ys = [x.get_distance_highest() for x in simulation.results]
 				else:
 					ys = [x.highest_fitness for x in simulation.results]
-				label = "{} Ind., {}, {}, {} ({}%), {}".format(simulation.population_size,
-															   get_marriage_name(simulation.marriage),
-															   get_crossover_name(simulation.crossover),
-															   get_mutation_name(simulation.mutation),
-															   simulation.mutation_rate,
-															   get_selection_name(simulation.selection))
+				label: str = str(simulation)
 				# if default color cycle provided by matplotlib is not enough, use custom defined colors
 				plt.plot(xs, ys, marker=".", label=label)
 			all_generations: List[int] = list(map(lambda sr: sr.generation, list(
