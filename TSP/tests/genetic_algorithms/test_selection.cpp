bf3a2903f63a3e9bfba868089b5e510f28c7fca9
nhrt <hartingerniklas@gmail.com>
2021-01-20 16:16:03 +0100

    removed error

--- TSP/tests/genetic_algorithms/test_selection.cpp
+++ TSP/tests/genetic_algorithms/test_selection.cpp
@@ -39,23 +39,37 @@ SCENARIO("Test Genetic Algorithms selection sotf distinct", "[genetic_algorithms
     std::vector<std::vector<int>> distances;
     read_distances(location + file_distances, distances);
 
-    for (int i = 0; i < 100; ++i) {
-        Population population1 = Population(5, 5, 1, rating_reversed, fitness_reversed, distances);
-        Population population2 = Population(5, 5, 1, rating_reversed, fitness_reversed, distances);
-        Population p_best = selection_sotf_distinct(population1, population2);
+    Population population1 = Population( 1, distances);
+    Population population2 = Population( 1, distances);
+    Individual i = Individual(5, 1, rating_reversed, fitness_reversed, false);
+    std::vector<int> chromosome = {5,2,0,4,3};
+    i.update_chromosome(chromosome);
+    population1.add_individual(i);
+    population1.add_individual(i);
 
-        for (int j = 0; j < p_best.size(); ++j) {
-            for (int k = 0; j < p_best.size(); ++j) {
+    population2.add_individual(i);
+    population2.add_individual(i);
+    Population p_best = selection_sotf_distinct(population1, population2);
+
+    for (unsigned int j = 0; j < p_best.size(); ++j) {
+        for (unsigned int k = 0; j < p_best.size(); ++j) {
+            if(j != k){
+                REQUIRE(p_best.get_individuals().at(j).get_chromosome() != p_best.get_individuals().at(k).get_chromosome());
+            }
+        }
+    }
+
+    for (int i = 0; i < 100; ++i) {
+        population1 = Population(5, 5, 1, rating_reversed, fitness_reversed, distances);
+        population2 = Population(5, 5, 1, rating_reversed, fitness_reversed, distances);
+        p_best = selection_sotf_distinct(population1, population2);
+
+        for (unsigned int j = 0; j < p_best.size(); ++j) {
+            for (unsigned int k = 0; j < p_best.size(); ++j) {
                 if(j != k){
-                    if(p_best.get_individuals().at(j).get_chromosome() != p_best.get_individuals().at(k).get_chromosome()){
-                        std::cout << "here";
-                    }
                     REQUIRE(p_best.get_individuals().at(j).get_chromosome() != p_best.get_individuals().at(k).get_chromosome());
                 }
             }
         }
-
     }
-
-
 }
